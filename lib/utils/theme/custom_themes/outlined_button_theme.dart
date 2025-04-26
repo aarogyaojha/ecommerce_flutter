046ef30322ba4accb0fc86a930d23eac8edae3c7
aarogyaojha <arogyaojha251@gmail.com>
2025-04-26 11:04:27 +0530

    outlinedbuttontheme

--- lib/utils/theme/custom_themes/outlined_button_theme.dart
+++ lib/utils/theme/custom_themes/outlined_button_theme.dart
@@ -1,3 +1,36 @@
+import 'package:flutter/material.dart';
+
 class TOutlinedButtonTheme {
-  
+  TOutlinedButtonTheme._();
+
+  //Light Theme
+  static final lightOutlinedButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.black,
+      side: BorderSide(color: Colors.blue),
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      textStyle: TextStyle(
+        fontSize: 16,
+        color: Colors.black,
+        fontWeight: FontWeight.w600,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    ),
+  );
+
+  static final darkOutlinedButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      side: BorderSide(color: Colors.blue),
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      textStyle: TextStyle(
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    ),
+  );
 }
