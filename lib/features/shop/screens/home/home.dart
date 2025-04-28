867d880de92fa624343ec846f8f90493f1532a6d
aarogyaojha <arogyaojha251@gmail.com>
2025-04-28 14:58:17 +0530

    app bar and refactoring

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_heade_container.dart';
+import 'widgets/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,7 +10,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [TPrimaryHeaderContainer(child: Container())]),
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(child: Column(children: [THomeAppBar()])),
+          ],
+        ),
       ),
     );
   }
