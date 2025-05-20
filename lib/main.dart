893bf96c540c2c4e8d8f356809cb60e2be791747
aarogyaojha <arogyaojha251@gmail.com>
2025-05-20 13:23:00 +0530

    integrated firebase with project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:ecommerce_flutter/app.dart';
+import 'package:ecommerce_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
+  // TODO: Add Widgets Binding---------------------------------------------------
+  // TODO: Init Local Strorage---------------------------------------------------
+  // TODO: Init Payment Methods---------------------------------------------------
+  // TODO: Await Native Splash---------------------------------------------------
+  // Initialize Firebase---------------------------------------------------
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+  
+  // TODO: Initialize Authetication---------------------------------------------------
+
   runApp(const App());
 }
