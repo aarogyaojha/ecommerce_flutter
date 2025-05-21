e20b063bca79dfcf5ac0b4bac2938837ee7e4331
aarogyaojha <arogyaojha251@gmail.com>
2025-05-21 10:09:24 +0530

    creating loader

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_flutter/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:ecommerce_flutter/utils/constants/colors.dart';
 import 'package:ecommerce_flutter/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -13,7 +14,10 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
-      home: OnboardingScreen(),
+      home: Scaffold(
+        backgroundColor: TColors.primary,
+        body: Center(child: CircularProgressIndicator(color: Colors.white)),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,26 @@ import 'package:ecommerce_flutter/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 Future<void> main() async {
-  // TODO: Add Widgets Binding---------------------------------------------------
-  // TODO: Init Local Strorage---------------------------------------------------
+  // Widgets Binding---------------------------------------------------
+
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+      
+  // Getx Local Strorage---------------------------------------------------
+  await GetStorage.init();
+
   // TODO: Init Payment Methods---------------------------------------------------
   // TODO: Await Native Splash---------------------------------------------------
+
   // Initialize Firebase---------------------------------------------------
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   ).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
-  
+
   // TODO: Initialize Authetication---------------------------------------------------
 
   runApp(const App());
